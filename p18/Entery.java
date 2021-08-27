8576d71cfb68bc674ea0dd05bf7a9544d319d398
saurabh211193 <saurabhnov93dtu@gmail.com>
2021-08-27 14:09:49 +0530

    Update Entery.java

--- p18/Entery.java
+++ p18/Entery.java
@@ -9,3 +9,24 @@ class Course
   List<Student> students;
   
 }
+
+Solution:
+
+class Student
+{
+  List<Course> courses;
+  
+}
+
+class Course
+{
+  List<Student> students;
+  
+}
+
+class Register{
+  List<Course> courses;
+  List<Student> students;
+  map<sid,List<courses>> ;
+  map<cid, List<students>> ;
+}
